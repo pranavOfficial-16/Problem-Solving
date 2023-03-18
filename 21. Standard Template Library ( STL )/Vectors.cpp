468df6ee29ad839fd1840d5de117a477776e3cc5
pranav_rao <pranavraoofficial@gmail.com>
2023-03-18 23:43:35 +0530

    commit

--- 21. Standard Template Library ( STL )/Vectors.cpp
+++ 21. Standard Template Library ( STL )/Vectors.cpp
@@ -3,8 +3,45 @@
 using namespace std;
 int main()
 {
+    vector<int> v;
+
+    for (int i = 0; i < 10; i++)
+    {
+        v.push_back(i); // inserting elements in the vector
+    }
+
+    cout << "the elements in the vector: ";
+    for (auto it = v.begin(); it != v.end(); it++)
+        cout << *it << " ";
+
+    cout << "\nThe front element of the vector: " << v.front();
+    cout << "\nThe last element of the vector: " << v.back();
+    cout << "\nThe size of the vector: " << v.size();
+    cout << "\nDeleting element from the end: " << v[v.size() - 1];
+    v.pop_back();
+
+    cout << "\nPrinting the vector after removing the last element:" << endl;
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+
+    cout << "\nInserting 5 at the beginning:" << endl;
+    v.insert(v.begin(), 5);
+    cout << "The first element is: " << v[0] << endl;
+    cout << "Erasing the first element" << endl;
+    v.erase(v.begin());
+    cout << "Now the first element is: " << v[0] << endl;
+
+    if (v.empty())
+        cout << "\nvector is empty";
+    else
+        cout << "\nvector is not empty" << endl;
+
+    v.clear();
+    cout << "Size of the vector after clearing the vector: " << v.size();
+
     // dynamic
     vector<int> *dynamic = new vector<int>();
+
     // static
     vector<int> v1;
     v1.push_back(10);
@@ -20,25 +57,25 @@ int main()
     for (auto i : v1)
         cout << i << endl;
     // static creation of vector
-    vector<int> v;
+    vector<int> v2;
     /*
         dynamic creation of vector
         vector<int> *vp = new vector<int>();
     */
     for (int i = 1; i <= 5; i++)
-        v.push_back(i);
+        v2.push_back(i);
     cout << "Output of begin and end: ";
     for (auto i = v.begin(); i != v.end(); i++)
         cout << *i << " ";
-    v.pop_back();
+    v2.pop_back();
     cout << "\nOutput after pop: ";
-    for (auto i = v.begin(); i != v.end(); ++i)
+    for (auto i = v2.begin(); i != v2.end(); ++i)
         cout << *i << " ";
     cout << "\nOutput of rbegin and rend: ";
-    for (auto ir = v.rbegin(); ir != v.rend(); ++ir)
+    for (auto ir = v2.rbegin(); ir != v2.rend(); ++ir)
         cout << *ir << " ";
     cout << "\nOutput of crbegin and crend : ";
-    for (auto ir = v.crbegin(); ir != v.crend(); ++ir)
+    for (auto ir = v2.crbegin(); ir != v2.crend(); ++ir)
         cout << *ir << " ";
     // 2d vectors
     vector<vector<int>> vect{

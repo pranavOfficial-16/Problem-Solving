bb20777dec74a0cb289d10af2ae1da81073064a3
pranav_rao <pranavraoofficial@gmail.com>
2023-03-23 19:07:21 +0530

    commit

--- 19. Dynamic Programming/FibonacciNumber.cpp
+++ 19. Dynamic Programming/FibonacciNumber.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int solve(int n, vector<int> &ans)
+
+// Brute force approach -> TC - 2^n
+int solve1(int n)
+{
+    if (n == 0 || n == 1)
+        return n;
+    return solve1(n - 1) + solve1(n - 2);
+}
+
+// Memoization -> ( Top-down approach ) ( Recursive ) TC - O(n)
+int solve2(int n, vector<int> &ans)
 {
     if (n == 0 || n == 1)
         return n;
@@ -10,8 +20,8 @@ int solve(int n, vector<int> &ans)
         return ans[n];
 
     // Calculate output
-    int a = solve(n - 1, ans);
-    int b = solve(n - 2, ans);
+    int a = solve2(n - 1, ans);
+    int b = solve2(n - 2, ans);
 
     // Save the output for future use
     ans[n] = a + b;
@@ -22,11 +32,26 @@ int solve(int n, vector<int> &ans)
 int fibo(int n)
 {
     vector<int> ans(n + 1, -1);
-    return solve(n, ans);
+    return solve2(n, ans);
 }
+
+// Dynamic programming -> ( Bottom-up approach) ( Iterative ) TC - O(n)
+int solve3(int n)
+{
+    int *ans = new int[n + 1];
+
+    ans[0] = 0;
+    ans[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        ans[i] = ans[i - 1] + ans[i - 2];
+
+    return ans[n];
+}
+
 int main()
 {
     int n;
     cin >> n;
-    cout << fibo(n) << endl;
+    cout << solve3(n) << endl;
 }

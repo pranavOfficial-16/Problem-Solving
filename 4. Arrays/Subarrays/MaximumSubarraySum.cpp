ccf651279a5778cde63ab58daf80cabcdc321962
pranav_rao <pranavraoofficial@gmail.com>
2023-03-24 19:14:39 +0530

    commit

--- 4. Arrays/Subarrays/MaximumSubarraySum.cpp
+++ 4. Arrays/Subarrays/MaximumSubarraySum.cpp
@@ -5,8 +5,8 @@ void solve(int arr[], int n)
 {
     int maxi = INT_MIN;
     for (int i = 0; i < n; i++)
-    {
-        for (int j = i; j < n; j++)
+    { 
+        for (int j = i; j < n ; j++)
         {
             int sum = 0;
             for (int k = i; k <= j; k++)

ab85a73c1b5d52ac95d87a86f90b2f87ce0bb25f
PRANAV G <pranavraoofficial@gmail.com>
2023-03-27 08:58:32 +0530

    Update patterns1.cpp

--- 1. Basic/patterns1.cpp
+++ 1. Basic/patterns1.cpp
@@ -604,3 +604,54 @@
 //     printSquare(n);
 //     return 0;
 // }
+
+/*
+    1
+    2*3
+    4*5*6
+    7*8*9*10
+    11*12*13*14*15
+    11*12*13*14*15
+    7*8*9*10
+    4*5*6
+    2*3
+    1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> v; 
+    // upper half
+    int count = 1;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(j==1)
+                v.push_back(count);
+            if(i==j)
+                cout<<count;
+            else
+                cout<<count<<"*";
+            count++;
+        }
+        cout<<endl;
+    }
+    // lower half
+    int m = v.size()-1;
+    int num;
+    for(int i=n;i>=1;i--){
+        num = v[m];
+        for(int j=1;j<=i;j++){
+            if(i == j)
+                cout<<num;
+            else
+                cout<<num<<"*";
+            num++;
+        }
+        m--;
+        cout<<endl;
+    }
+    return 0;
+}

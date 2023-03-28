e51cb64a404bdae9f0094b7ac190bc46b6228970
pranav_rao <pranavraoofficial@gmail.com>
2023-03-28 22:55:49 +0530

    c

--- 19. Dynamic Programming/MinCostPath.cpp
+++ 19. Dynamic Programming/MinCostPath.cpp
@@ -22,6 +22,56 @@ int minCostPath(int **input, int m, int n)
     return minCostPath(input, m, n, 0, 0);
 }
 
+// Memoization
+int minCostPath1(int **input, int m, int n, int i, int j, int **output)
+{
+    if (i == m - 1 && j == n - 1)
+        return input[i][j];
+
+    if (i >= m || j >= n)
+        return INT_MAX;
+
+    if (output[i][j] != -1)
+        return output[i][j];
+
+    int x = minCostPath1(input, m, n, i, j + 1, output);
+    int y = minCostPath1(input, m, n, i + 1, j + 1, output);
+    int z = minCostPath1(input, m, n, i + 1, j, output);
+
+    int ans = min(x, min(y, z)) + input[i][j];
+
+    output[i][j] = ans;
+
+    return ans;
+}
+
+// dp
+int minCostPath2(int **input, int m, int n)
+{
+    int **ans = new int *[m];
+    for (int i = 0; i < m; i++)
+        ans[i] = new int[n];
+
+    ans[m - 1][n - 1] = input[m - 1][n - 1];
+
+    // Last row
+    for (int j = n - 2; j >= 0; j--)
+        ans[m - 1][j] = input[m - 1][j] + ans[m - 1][j + 1];
+
+    // Last col
+    for (int i = m - 2; i >= 0; i--)
+        ans[i][n - 1] = input[i][n - 1] + ans[i + 1][n - 1];
+
+    for (int i = m - 2; i >= 0; i--)
+    {
+        for (int j = n - 2; j >= 0; j--)
+        {
+            ans[i][j] = input[i][j] + min(ans[i][j + 1], min(ans[i + 1][j + 1], ans[i + 1][j]));
+        }
+    }
+    return ans[0][0];
+}
+
 int main()
 {
     int **arr, n, m;

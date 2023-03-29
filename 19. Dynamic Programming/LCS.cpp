bb41321dd25983b1a8abd93671863e4c62b32a8f
pranav_rao <pranavraoofficial@gmail.com>
2023-03-29 23:49:33 +0530

    commit

---
+++ 19. Dynamic Programming/LCS.cpp
@@ -0,0 +1,26 @@
+// Longest common subsequence
+#include <bits/stdc++.h>
+using namespace std;
+int LCS(string s, string t)
+{
+    // base case
+    if (s.size() == 0 || t.size() == 0)
+        return 0;
+
+    if (s[0] == t[0])
+        return 1 + LCS(s.substr(1), t.substr(1));
+    else
+    {
+        int a = LCS(s.substr(1), t);
+        int b = LCS(s, t.substr(1));
+        int c = LCS(s.substr(1), t.substr(1));
+        return max(a, max(b, c));
+    }
+}
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    cout << LCS(s, t) << endl;
+    return 0;
+}

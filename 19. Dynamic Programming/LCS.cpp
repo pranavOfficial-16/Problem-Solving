d28efc293d2b086f9c5cee7f3e56c2bd4965c02c
pranav_rao <pranavraoofficial@gmail.com>
2023-03-31 01:02:52 +0530

    commit

--- 19. Dynamic Programming/LCS.cpp
+++ 19. Dynamic Programming/LCS.cpp
@@ -1,26 +1,131 @@
 // Longest common subsequence
 #include <bits/stdc++.h>
 using namespace std;
+
+// bruteforce
 int LCS(string s, string t)
 {
-    // base case
-    if (s.size() == 0 || t.size() == 0)
-        return 0;
+	// base case
+	if (s.size() == 0 || t.size() == 0)
+		return 0;
 
-    if (s[0] == t[0])
-        return 1 + LCS(s.substr(1), t.substr(1));
-    else
-    {
-        int a = LCS(s.substr(1), t);
-        int b = LCS(s, t.substr(1));
-        int c = LCS(s.substr(1), t.substr(1));
-        return max(a, max(b, c));
-    }
+	if (s[0] == t[0])
+		return 1 + LCS(s.substr(1), t.substr(1));
+	else
+	{
+		int a = LCS(s.substr(1), t);
+		int b = LCS(s, t.substr(1));
+		int c = LCS(s.substr(1), t.substr(1));
+		return max(a, max(b, c));
+	}
 }
+
+// memoization
+int lcs_mem(string s, string t, int **output)
+{
+
+	int m = s.size();
+	int n = t.size();
+
+	// Base case
+	if (s.size() == 0 || t.size() == 0)
+	{
+		return 0;
+	}
+
+	// Check if ans already exists
+	if (output[m][n] != -1)
+	{
+		return output[m][n];
+	}
+
+	int ans;
+	// Recursive calls
+	if (s[0] == t[0])
+	{
+		ans = 1 + lcs_mem(s.substr(1), t.substr(1), output);
+	}
+	else
+	{
+		int a = lcs_mem(s.substr(1), t, output);
+		int b = lcs_mem(s, t.substr(1), output);
+		int c = lcs_mem(s.substr(1), t.substr(1), output);
+		ans = max(a, max(b, c));
+	}
+
+	// Save your calculation
+	output[m][n] = ans;
+
+	// Return ans
+	return ans;
+}
+int lcs_mem(string s, string t)
+{
+	int m = s.size();
+	int n = t.size();
+	int **output = new int *[m + 1];
+	for (int i = 0; i <= m; i++)
+	{
+		output[i] = new int[n + 1];
+		for (int j = 0; j <= n; j++)
+		{
+			output[i][j] = -1;
+		}
+	}
+
+	return lcs_mem(s, t, output);
+}
+
+// dp
+int lcs_DP(string s, string t)
+{
+	int m = s.size();
+	int n = t.size();
+	int **output = new int *[m + 1];
+
+	for (int i = 0; i <= m; i++)
+	{
+		output[i] = new int[n + 1];
+	}
+
+	// Fill 1st row
+	for (int j = 0; j <= n; j++)
+	{
+		output[0][j] = 0;
+	}
+
+	// Fill 1st col
+	for (int i = 1; i <= m; i++)
+	{
+		output[i][0] = 0;
+	}
+
+	for (int i = 1; i <= m; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			// Check if 1st char matches
+			if (s[m - i] == t[n - j])
+			{
+				output[i][j] = 1 + output[i - 1][j - 1];
+			}
+			else
+			{
+				int a = output[i - 1][j];
+				int b = output[i][j - 1];
+				int c = output[i - 1][j - 1];
+				output[i][j] = max(a, max(b, c));
+			}
+		}
+	}
+
+	return output[m][n];
+}
+
 int main()
 {
-    string s, t;
-    cin >> s >> t;
-    cout << LCS(s, t) << endl;
-    return 0;
+	string s, t;
+	cin >> s >> t;
+	cout << LCS(s, t) << endl;
+	return 0;
 }

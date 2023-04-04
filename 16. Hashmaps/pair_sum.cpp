d131393c9731decd59197ed49f22885a2de14861
pranav_rao <pranavraoofficial@gmail.com>
2023-04-04 22:24:38 +0530

    commit

--- 16. Hashmaps/pair_sum.cpp
+++ 16. Hashmaps/pair_sum.cpp
@@ -2,20 +2,19 @@
 using namespace std;
 int pairSum(int *arr, int n)
 {
-    int sum = 0;
+    int sum = 6;
     unordered_map<int, int> map;
     int count = 0;
     for (int i = 0; i < n; i++)
     {
-        int value = sum - arr[i];
-        if (map[value])
-            count += map[value];
+        if (map.find(sum - arr[i]) != map.end())
+            count += map[sum - arr[i]];
         map[arr[i]]++;
     }
     return count;
 }
 int main()
 {
-    int arr[] = {2, 1, -2, 2, 3};
+    int arr[] = {1, 5, 7, -1, 5};
     cout << pairSum(arr, 5);
 }

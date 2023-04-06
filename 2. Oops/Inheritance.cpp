72979d072e872ad659129cc12d9ccd63e9ddc884
pranav_rao <pranavraoofficial@gmail.com>
2023-04-06 23:40:11 +0530

    commit

---
+++ 2. Oops/Inheritance.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+class vehicle
+{
+private:
+    int a;
+
+protected:
+    int b;
+
+public:
+    int c;
+};
+class car : public vehicle
+{
+    int d;
+};
+int main()
+{
+    return 0;
+}

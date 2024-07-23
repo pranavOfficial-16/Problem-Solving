0884316d5660c963921d5829a37bcf755579a5cb
pranav_rao <pranavraoofficial@gmail.com>
2024-07-23 16:57:54 +0530

    commit

---
+++ 4. Arrays/leadersinanarray.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> leaders(int n, int arr[]) {
+   vector<int> ans;
+   ans.push_back(arr[n - 1]);
+   int maxi = arr[n - 1];
+   for (int i = n - 2; i >= 0; i--) {
+      if (arr[i] >= maxi) {
+         ans.push_back(arr[i]);
+         maxi = arr[i];
+      }
+   }
+   reverse(ans.begin(), ans.end());
+   return ans;
+}

a1805b9922707e5b62f133874609df17998e24ad
Pranav G <pranavraoofficial@gmail.com>
2024-08-04 14:04:45 +0530

    commit

--- 4. Arrays/arraysum.cpp
+++ 4. Arrays/arraysum.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-   	int n,sum=0;
-    cin>>n;
+int main()
+{
+    int n, sum = 0;
+    cin >> n;
     int a[n];
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-        sum+=a[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
     }
-    cout<<sum;
+    cout << sum;
     return 0;
 }
-
-

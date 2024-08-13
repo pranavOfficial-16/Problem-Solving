5945846b0203121392b7fc9246c97aee93800611
Pranav G <pranavraoofficial@gmail.com>
2024-08-13 14:10:46 +0530

    commit

--- 4. Arrays/tripletsum.cpp
+++ 4. Arrays/tripletsum.cpp
@@ -35,31 +35,33 @@
 //  SC - O(1) , TC - o(n^2)
 #include <bits/stdc++.h>
 using namespace std;
-int tripletSum(int *a, int n, int sum)
+int tripletSum(int *a, int n, int k)
 {
-    int count = 0, s, e;
+    int count = 0, s, e, sum;
     sort(a, a + n);
     for (int i = 0; i < n; i++)
     {
+        if (i > 0 && a[i] == a[i - 1])
+            continue;
         s = i + 1;
         e = n - 1;
         while (s < e)
         {
-            if (a[i] + a[s] + a[e] == sum)
+            sum = a[i] + a[s] + a[e];
+            if (sum < 0)
+                s++;
+            else if (sum > 0)
+                e--;
+            else
             {
                 count++;
                 s++;
-                int temp = e;
-                while (a[temp - 1] == a[temp] && temp > s)
-                {
-                    count++;
-                    temp--;
-                }
-            }
-            else if (a[i] + a[s] + a[e] < sum)
-                s++;
-            else
                 e--;
+                while (s < e && a[s] == a[s - 1])
+                    s++;
+                while (s < e && a[e] == a[e + 1])
+                    e--;
+            }
         }
     }
     return count;
@@ -71,8 +73,8 @@ int main()
     int *a = new int[n];
     for (int i = 0; i < n; i++)
         cin >> a[i];
-    int x;
-    cin >> x;
-    cout << tripletSum(a, n, x);
+    int k;
+    cin >> k;
+    cout << tripletSum(a, n, k);
     return 0;
 }

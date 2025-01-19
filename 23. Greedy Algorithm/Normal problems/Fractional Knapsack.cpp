c0680f8a2f9f0437436384d4cfdcd42d5b50e80e
Pranav G <pranavraoofficial@gmail.com>
2025-01-19 12:37:30 +0530

    commit

---
+++ 23. Greedy Algorithm/Normal problems/Fractional Knapsack.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+static bool comp(pair<int, int> a, pair<int, int> b)
+{
+    double r1 = (double)a.first / a.second;
+    double r2 = (double)b.first / b.second;
+    return r1 > r2;
+}
+double fractionalKnapsack(vector<int> &val, vector<int> &wt, int capacity)
+{
+    vector<pair<int, int>> arr;
+
+    for (int i = 0; i < val.size(); ++i)
+        arr.emplace_back(val[i], wt[i]);
+
+    sort(arr.begin(), arr.end(), comp);
+
+    double totalval = 0;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i].second <= capacity)
+        {
+            totalval += arr[i].first;
+            capacity = capacity - arr[i].second;
+        }
+        else
+        {
+            totalval += ((double)arr[i].first / arr[i].second) * capacity;
+            break;
+        }
+    }
+    return totalval;
+}
